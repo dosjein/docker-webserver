5f40658c700de2e75729f9dc2ec2e0d7d0059114
Ronalds Sovas <sovas@greenbox.lv>
2018-01-03 17:55:11 +0200

    added simple alias command and minor changes to heroku image behaviour

--- projects/heroku/start.sh
+++ projects/heroku/start.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 # ------------------------------------------
-# Set Up life VkScript
+# Create Trigger file
 # ------------------------------------------
 
 if [ ! -f /trigger.sh ]
 then
-    echo 'while true; do php /var/www/artisan vk:messages; sleep 1; done' > /trigger.sh
+	#Simple Heroku with no PreAuth
+	if [ ! -f ~/.netrc ]
+	then
+	    echo 'tail -f /dev/null' > /trigger.sh
+	fi
+
+	if [ -f ~/.netrc ]
+	then
+	    echo 'cd /opt; while true; do git push heroku master; sleep 300; done' > /trigger.sh
+	fi
     chmod -fR 777 /trigger.sh
 fi
 
